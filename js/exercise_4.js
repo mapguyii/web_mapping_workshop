d4025e8629100a75acb9921458da8c2fd205fe21
mapguyii <mapguy@aol.com>
2016-05-02 15:22:29 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -33,4 +33,11 @@ featureLayer.on('ready', function(){
     })
     map.fitBounds(featureLayer.getBounds());
 })
+
+featureLayer.on('ready', function(){
+	this.eachLayer(function(layer){
+    	layer.bindPopup('Welcome to ' + layer.feature.properties.name)
+    })
+
+})
 	
